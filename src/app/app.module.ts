67d9cbdaea8f8231ed85ba66082bc42375fc11ef
waelby99 <waelbenyoussef19991@gmail.com>
2024-02-14 23:46:20 +0100

    3rd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OffreComponent } from './offre/offre.component';
@@ -12,6 +12,7 @@ import { OffreComponent } from './offre/offre.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
