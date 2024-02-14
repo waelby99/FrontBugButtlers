a4a383998e55bbd4b226b6bb242393c6e64ffa11
waelby99 <waelbenyoussef19991@gmail.com>
2024-02-14 22:22:27 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OffreComponent } from './offre/offre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OffreComponent
   ],
   imports: [
     BrowserModule,
